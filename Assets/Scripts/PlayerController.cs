65e0169ec59656e1f8d4b4095a9e09d50f870afb
OmerZamir123 <62934105+OmerZamir123@users.noreply.github.com>
2026-01-30 14:42:38 +0200

    add mask and disapperness when colide with player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,18 +4,11 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float currentSpeed = 5f;
-    [SerializeField] float steerSpeed = 200f;
+    [SerializeField] float steerSpeed = 5f;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        Debug.Log("Hello");
-    }
-
-    // Update is called once per frame
     void Update()
     {
-                float move = 0f;
+        float move = 0f;
         float steer = 0f;
 
         if (Keyboard.current.wKey.isPressed)
@@ -29,17 +22,24 @@ public class PlayerController : MonoBehaviour
 
         if (Keyboard.current.aKey.isPressed)
         {
-            steer = 1f;
+            steer = -1f;
         }
         else if (Keyboard.current.dKey.isPressed)
         {
-            steer = -1f;
+            steer = 1f;
         }
 
         float moveAmount = move * currentSpeed * Time.deltaTime;
         float steerAmount = steer * steerSpeed * Time.deltaTime;
 
         transform.Translate(0, moveAmount, 0);
-        transform.Rotate(0, 0, steerAmount);
+        transform.Translate(steerAmount, 0, 0);
+        //transform.Rotate(0, 0, steerAmount);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("What?!");
+        Destroy(collision.gameObject);
     }
 }
