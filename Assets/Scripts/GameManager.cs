d65ce42a1f6a7a53d2dd0a12a50f12126d652c1b
OmerZamir123 <62934105+OmerZamir123@users.noreply.github.com>
2026-01-30 18:22:33 +0200

    Replace player box in an image, also spwaning the masks not on the player

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,14 +3,32 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public ObjectPool pool;
+    public GameObject player;
 
     const int NUM_OF_MASKS = 10;
 
     void Start()
     {
+        Vector3 playerPosition = player.transform.position;
+
         for(int i = 0; i < NUM_OF_MASKS; i++)
         {
-            pool.GetObject().transform.Translate(Random.Range(-5,6), Random.Range(-5,6), 0);
+            GameObject obj = pool.GetObject();
+
+            int xRandom = UnityEngine.Random.Range(0, 2) == 0
+                        ? UnityEngine.Random.Range(-10, -2)
+                        : UnityEngine.Random.Range(3, 11);
+
+            int yRandom = UnityEngine.Random.Range(0, 2) == 0
+                        ? UnityEngine.Random.Range(-10, -2)
+                        : UnityEngine.Random.Range(3, 11);
+
+
+            float moveX = playerPosition.x + xRandom;
+            float moveY = playerPosition.y + yRandom;
+
+            obj.transform.Translate(moveX, moveY, 0);
+            obj.GetComponent<SpriteRenderer>().color = Random.ColorHSV();
         }
     }
 }
